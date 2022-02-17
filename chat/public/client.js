1b39400a138535ac9a6143ece6869c7172b784aa
nikhil-pal-au16 <nikhilpal089@gmail.com>
2022-02-17 15:42:19 +0530

    "chatapp"

---
+++ chat/public/client.js
@@ -0,0 +1,56 @@
+const socket = io();
+
+let Name;
+let textarea = document.querySelector('#textarea');
+let messageArea = document.querySelector('.message_area')
+
+do {
+    Name = prompt('please Enter Your Name: ')
+} while (!Name);
+
+textarea.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: Name,
+        message: message.trim()
+    }
+    // Append
+    appendMessage(msg, 'outgoing');
+    textarea.value = ''
+    scrollToBottom();
+
+    // send to server
+    socket.emit('message', msg)
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+
+    mainDiv.innerHTML = markup
+
+    messageArea.appendChild(mainDiv)
+}
+
+// Receive message
+
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming');
+    scrollToBottom();
+})
+
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
